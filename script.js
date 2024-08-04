7760b4b5730a0bf51d99e9b5aa3c7521b6173b5b
klhaug <kristian@snougle.no>
2024-08-04 13:45:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var ul = document.getElementsByTagName("ul");
+var liWrapper = document.getElementsByClassName("li-wrapper");
+var li = document.getElementsByTagName("li");
+var button = document.getElementsByClassName("del-btn");
+var input = document.getElementById("userinput");
+var enter = document.getElementById("enter");
+
+function getInputTxt() {
+    console.log("hi");
+}
+
+
+
+var submitBtn = enter.addEventListener("click", consoleHi(tull))
+
+function consoleHi(a){
+    console.log(a);
+}
+
+getInputTxt();
+
+
+
+
+// It listens for events, then executes. 
